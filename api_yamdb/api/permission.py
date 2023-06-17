d7b46f9b38cc931402d4876c4053a0f5a3fe9e62
Andrey-66 <andrey66-66-66@yandex.ru>
2023-06-17 21:14:47 +0300

    Delete conditions in permission.py

--- api_yamdb/api/permission.py
+++ api_yamdb/api/permission.py
@@ -23,20 +23,12 @@ class AdminOrReadOnly(BasePermission):
 
 class AuthorOrModerOrReadOnly(BasePermission):
     def has_object_permission(self, request, view, obj):
-        if request.user.is_superuser:
-            return True
-        if request.method in SAFE_METHODS:
-            return True
-        user = request.user
-
-        if not user.is_authenticated:
-            return False
-
-        if user.role == 'admin' or user.role == 'moderator':
-            return True
-
-        author = obj.author
-        if author == user:
-            return True
-
-        return False
+        return (
+            request.method in SAFE_METHODS or (
+                request.user.is_authenticated and (
+                    request.user.is_admin or
+                    request.user.is_moderator or
+                    request.user == obj.author
+                )
+            )
+        )
