0e1f96f851083c3b6fec89ae62eabc0288b5e478
Andrey-66 <andrey66-66-66@yandex.ru>
2023-07-06 18:50:07 +0300

    Fix admin.py

--- api_yamdb/users/admin.py
+++ api_yamdb/users/admin.py
@@ -1,17 +1,22 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 
-from .models import User
 
+class CustomUserAdmin(BaseUserAdmin):
+    list_display = ('username', 'email', 'is_staff', 'get_role_display')
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
+    search_fields = ('username', 'email')
+    ordering = ('username',)
+    filter_horizontal = ()
 
-class UserAdmin(BaseUserAdmin):
-    list_display = ('username', 'email', 'role')
-    list_filter = ('role', 'is_superuser')
     fieldsets = (
         (None, {'fields': ('username', 'password')}),
         ('Personal Info', {'fields': ('email', 'bio', 'confirmation_code')}),
-        ('Permissions', {'fields': ('role', 'is_staff', 'is_active')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Roles', {'fields': ('role',)}),
     )
+
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
@@ -20,15 +25,12 @@ class UserAdmin(BaseUserAdmin):
                 'email',
                 'password1',
                 'password2',
-                'role',
                 'is_staff',
-                'is_active'
-            )}
-         ),
+                'is_active',
+                'role',
+            ),
+        }),
     )
-    search_fields = ('username', 'email')
-    ordering = ('username',)
-    filter_horizontal = ()
 
 
-admin.site.register(User, UserAdmin)
+admin.site.register(User, CustomUserAdmin)
