95cc9b54a0a2d475ce71664f0d4b2ff17babdc3d
Andrey-66 <55087387+Andrey-66@users.noreply.github.com>
2023-07-23 14:02:08 +0300

    update validators.py

--- api_yamdb/users/validators.py
+++ api_yamdb/users/validators.py
@@ -7,16 +7,16 @@ from constants import USERNAME_PATTERN
 
 def regex_validator(value):
     if value.lower() == 'me':
-        raise ValidationError("Username 'me' is not allowed.")
+        raise ValidationError('Username "me" is not allowed.')
 
     invalid_chars = ''.join(set(re.sub(USERNAME_PATTERN, '', value)))
     if invalid_chars:
         error_message = (f"Username contains invalid characters:"
                          f" {invalid_chars}")
         raise ValidationError(error_message)
+    return value
 
 
 class UsernameValidationMixin:
     def validate_username(self, value):
-        regex_validator(value)
-        return value
+        return regex_validator(value)
